4a4480032a377c1b4144635cc66f7105fb2afc7d
roll <roll@post.agency>
2016-01-07 12:30:16 +0300

    rebased on new Storage edition

---
+++ examples/base.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import sys
+from sqlalchemy import create_engine
+
+sys.path.insert(0, '.')
+import jtssql
+import dpsql
+
+
+def run(url, prefix, source, target):
+
+    # Storage
+    engine = create_engine(url)
+    storage = jtssql.Storage(engine=engine, prefix=prefix)
+
+    # Import package
+    dpsql.import_package(storage, source)
+
+    # Export package
+    dpsql.export_package(storage, target)
